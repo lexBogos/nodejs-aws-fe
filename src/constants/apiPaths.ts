982bbb74026e0babd0d8c5586cc20226023f988c
Aliaksei Tamashevich <aliaksei.tamashevich@burberry.com>
2020-12-07 10:03:04 +0300

    feat: added cart endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://7ovvn6h9sj.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://lexbogos-cart-api-develop.eu-west-1.elasticbeanstalk.com/app',
 };
 
 export default API_PATHS;
